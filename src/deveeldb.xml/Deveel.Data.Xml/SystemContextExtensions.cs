b75f243c0d6d12dee9f316c1c945c40ba0d6f08a
Antonello Provenzano <antonello@deveel.com>
2016-03-23 21:18:57 +0100

    Move of the project from the main DeveelDB solution

---
+++ src/deveeldb.xml/Deveel.Data.Xml/SystemContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using Deveel.Data.Services;
+
+namespace Deveel.Data.Xml {
+	public static class SystemContextExtensions {
+		public static void UseXml(this ISystemContext context) {
+			context.RegisterService<XmlTypeResolver>();
+			context.RegisterInstance(XmlFunctions.Resolver);
+		}
+	}
+}
